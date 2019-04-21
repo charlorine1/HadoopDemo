cd9640d42a540b5b85f2199ca786bc10156a1cc1
yangchangxing <ycxusst@163.com>
2019-04-22 00:08:47 +0800

    new_install_user map

---
+++ src/com/usst/transformer/model/value/BaseStatsValueWritable.java
@@ -0,0 +1,22 @@
+package com.usst.transformer.model.value;
+
+import org.apache.hadoop.io.Writable;
+
+import com.usst.commom.KpiType;
+
+
+/**
+ * 自定义顶级的输出value父类
+ * 
+ * @author root
+ *
+ */
+public abstract class BaseStatsValueWritable implements Writable {
+    /**
+     * 获取当前value对应的kpi值
+     * 
+     * @return
+     */
+    public abstract KpiType getKpi();
+}
+
---
+++ src/com/usst/transformer/model/value/map/TimeOutputValue.java
@@ -0,0 +1,30 @@
+package com.usst.transformer.model.value.map;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import com.usst.commom.KpiType;
+import com.usst.transformer.model.value.BaseStatsValueWritable;
+
+public class TimeOutputValue extends BaseStatsValueWritable{
+
+	@Override
+	public void readFields(DataInput arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void write(DataOutput arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public KpiType getKpi() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
